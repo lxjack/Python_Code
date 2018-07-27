9c03c1aeaf1fd14d83338af686336330ac545fe8
lxjack <2292471222@qq.com>
2018-07-27 11:11:18 +0800

    add html report

--- data_struct/Llist/testcase_schedule.py
+++ data_struct/Llist/testcase_schedule.py
@@ -1,16 +1,19 @@
 #_*_ coding: utf-8 _*_
 import unittest
 from link_list_test import LlistTest
+import HTMLTestRunner
 
 if __name__ == '__main__':
     '''用例的调度组织'''
     suite = unittest.TestSuite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(LlistTest))
 
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(suite)
+    fp = file('my_report.html', 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+        stream=fp,
+        title='My unit test',
+        description='This demonstrates the report output by HTMLTestRunner.'
+    )
 
-    #将测试结果输出到文件"e:\\UnittestTextReport.txt"
-    # with open('e:\\UnittestTextReport.txt', 'a') as f:
-    #     runner = unittest.TextTestRunner(stream=f, verbosity=2)
-    #     runner.run(suite)
+    runner.STYLESHEET_TMPL = '<link rel="stylesheet" href="my_stylesheet.css" type="text/css">'
+    runner.run(suite)
